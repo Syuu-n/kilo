627ff7fdee3ee3843a7242e18da12d54547ce925
NobuhiroKato <n-kato@nekojarashi.com>
2020-04-25 22:04:38 +0900

    Fix seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,13 +1,13 @@
 # coding: utf-8
 
-unless Plan.first.exist?
+unless Plan.exists?
   Plan.create(
     name: '大人クラス',
     price: 3000
   )
 end
 
-unless User.first.exist?
+unless User.exists?
   User.create(
     name: '加藤 修宏',
     name_kana: 'カトウ ノブヒロ',
