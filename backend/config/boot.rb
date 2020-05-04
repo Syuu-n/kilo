251d6891569ee8e8f67207afb73936aa4f5cab86
NobuhiroKato <n-kato@nekojarashi.com>
2020-05-04 14:38:14 +0900

    Hide warnings

--- backend/config/boot.rb
+++ backend/config/boot.rb
@@ -1,3 +1,7 @@
+# TODO: Ruby 2.7 で現れる大量の Warning を非表示にする
+# 有用な Warning も非表示にしてしまう可能性があり
+Warning[:deprecated] = false
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
