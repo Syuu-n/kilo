2e92fa71ff4e6eb25f8e394c8c38d0a4ccfe9e59
NobuhiroKato <n-kato@nekojarashi.com>
2020-06-26 01:43:33 +0900

    Fix history

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import { createBrowserHistory } from 'history';
+import hist from 'RouterHistory';
 import * as React from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 import { AuthProvider } from 'Auth';
 import MainPage from 'layouts/MainPageLayout';
 import LoginView from 'views/LoginView';
-
-const hist = createBrowserHistory();
 
 // AuthProvider 配下は認証が必要
 const App: React.FC = () => {
