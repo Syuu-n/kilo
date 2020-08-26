0cb9519a89dd942b96d1ff5f6180b3a0017c6ee5
Aura-n <reikage46497@gmail.com>
2020-08-26 10:34:58 +0900

    Add notistack

--- frontend/src/components/Modal/PasswordResetModal.tsx
+++ frontend/src/components/Modal/PasswordResetModal.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Modal, Snackbar } from 'components';
+import { Modal } from 'components';
 import { fetchApp, NetworkError } from 'request/fetcher';
-import { MailOutline } from '@material-ui/icons';
+import { useSnackbar, VariantType } from 'notistack';
 
 interface Props {
   open: boolean;
@@ -11,69 +11,52 @@ interface Props {
 
 const PasswordResetModal: React.FC<Props> = (props) => {
   const { open, closeFunc, email } = props;
-  const [notification, setNotification] = React.useState(false);
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   const content = 
     <p>
       登録されているメールアドレスにパスワード変更のメールを送信します。<br/>
       メールからパスワード変更をおこなうとパスワードが変更できます。
     </p>;
 
-  const showNotification = () => {
-    setNotification(true)
-    setTimeout(() => setNotification(false), 8000);
+  const showNotification = (message: string, variant?: VariantType) => {
+    enqueueSnackbar(message, { variant });
+    setTimeout(() => closeSnackbar(), 8000);
   }
 
   const handleSubmit = async () => {
-    const accessToken = localStorage.getItem('kiloToken');
-    if (accessToken) {
-      const res = await fetchApp(
-        '/v1/passwords',
-        'POST',
-        accessToken,
-        JSON.stringify({
-          user: {
-            email: email,
-          },
-        })
-      )
-      if (res instanceof NetworkError) {
-        console.log('予期せぬエラーが発生しました。時間をおいて再度お試しください。');
-        return
-      }
-      switch (res.status) {
-        case 200:
-          console.log('送信しました。')
-          showNotification()
-          break;
-        default:
-          console.log('予期せぬエラーが発生しました。時間をおいて再度お試しください。');
-      }
-    } else {
-      console.log('Access token not found')
+    const res = await fetchApp(
+      '/v1/passwords',
+      'POST',
+      '',
+      JSON.stringify({
+        user: {
+          email: email,
+        },
+      })
+    )
+    if (res instanceof NetworkError) {
+      console.log('NetworkError');
+      showNotification('予期せぬエラーが発生しました。時間をおいて再度お試しください。', 'error');
       return
+    }
+    switch (res.status) {
+      case 200:
+        showNotification('パスワード再設定用のメールを送信しました。', 'success');
+        break;
+      default:
+        showNotification('予期せぬエラーが発生しました。時間をおいて再度お試しください。', 'error');
     }
   }
 
   return (
-    <div>
-      <Modal
-        open={open}
-        headerTitle="パスワード変更"
-        content={content}
-        submitText="変更"
-        submitFunc={() => {handleSubmit()}}
-        closeFunc={closeFunc}
-      />
-      <Snackbar
-        place="br"
-        color="success"
-        message="パスワード再設定用のメールを送信しました。"
-        icon={MailOutline}
-        open={notification}
-        closeNotification={() => setNotification(false)}
-        close
-      />
-    </div>
+    <Modal
+      open={open}
+      headerTitle="パスワード変更"
+      content={content}
+      submitText="変更"
+      submitFunc={() => {handleSubmit()}}
+      closeFunc={closeFunc}
+    />
   );
 };
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,17 @@ import 'assets/css/material-dashboard-react.css?v=1.2.0';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from 'App';
+import { SnackbarProvider } from 'notistack';
 
 ReactDOM.render(
-  <App/>,
+  <SnackbarProvider
+    maxSnack={1}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+  >
+    <App/>
+  </SnackbarProvider>,
   document.getElementById('root'),
 );
