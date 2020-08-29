69c57fa30b161bd8106c9543501b2285111a77f1
Aura-n <reikage46497@gmail.com>
2020-08-30 00:02:23 +0900

    Add grid to ScheduleView

--- frontend/src/views/ScheduleView.tsx
+++ frontend/src/views/ScheduleView.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import { CalenderCard } from 'components';
+import { ItemGrid, CalenderCard } from 'components';
+import { Grid,} from '@material-ui/core';
 
 const ScheduleView: React.SFC = () => {
   return (
     <div>
-      <CalenderCard
-        cardTitle="スケジュール"
-      />
+      <Grid container>
+        <ItemGrid xs={12}>
+          <CalenderCard
+            cardTitle="スケジュール"
+          />
+        </ItemGrid>
+      </Grid>
     </div>
   );
 };
