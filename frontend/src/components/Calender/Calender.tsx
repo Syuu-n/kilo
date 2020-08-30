c7e8d574b81963ee9ae1f5401a2bc6d05b351dac
Aura-n <reikage46497@gmail.com>
2020-08-30 15:19:03 +0900

    Add calender style

--- frontend/src/components/Calender/Calender.tsx
+++ frontend/src/components/Calender/Calender.tsx
@@ -1,16 +1,27 @@
 import * as React from 'react';
-import { Calendar, momentLocalizer, Formats } from 'react-big-calendar';
+import { Calendar, momentLocalizer, Formats, Messages } from 'react-big-calendar';
 import * as moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../../assets/css/kilo-calender.css';
 import { Modal } from 'components';
 
 const localizer = momentLocalizer(moment);
 
 const formats:Formats = {
   dateFormat: 'D',
-  dayFormat: 'D(ddd)',
+  dayFormat: 'D (ddd)',
   monthHeaderFormat: 'YYYY年M月',
-  dayHeaderFormat: 'M月D日(ddd)',
+  dayHeaderFormat: 'M月D日 (ddd)',
+};
+
+const messages:Messages ={
+  today: '今日',
+  yesterday: '昨日',
+  tomorrow: '明日',
+  previous: '←',
+  next: '→',
+  month: '月',
+  week: '週',
+  day: '日',
 };
 
 const eventList = [
@@ -25,10 +36,20 @@ const eventList = [
     id: 1,
     title: 'Long Event',
     allDay: false,
-    start: new Date('2020-03-07 15:00'),
-    end: new Date('2020-03-07 17:00'),
+    start: new Date('2020-08-24 15:00'),
+    end: new Date('2020-08-24 17:00'),
   }
 ];
+
+// const eventColors = (event:Event) => {
+//   var backgroundColor = "event-";
+//   event.color
+//     ? (backgroundColor = backgroundColor + event.color)
+//     : (backgroundColor = backgroundColor + "default");
+//   return {
+//     className: backgroundColor
+//   };
+// };
 
 const Calender: React.FC = () => {
   const [openModal, setOpenModal] = React.useState(false);
@@ -47,7 +68,8 @@ const Calender: React.FC = () => {
         formats={formats}
         // onSelectEvent={event => alert(event.title)}
         onSelectEvent={() => addNewEventModal() }
-        style={{ height: 500 }}
+        messages={messages}
+        // eventPropGetter={eventColors}
       />
       <Modal
         open={openModal}
