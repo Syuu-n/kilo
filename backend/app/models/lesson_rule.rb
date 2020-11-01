c982f1edb3386112d327985f04ae4f996c17610f
Aura-n <reikage46497@gmail.com>
2020-11-01 19:19:25 +0900

    Add week to lesson_rules

--- backend/app/models/lesson_rule.rb
+++ backend/app/models/lesson_rule.rb
@@ -1,7 +1,10 @@
 class LessonRule < ApplicationRecord
   belongs_to :lesson_class
 
+  # week は週の何周目かを表す e.g. 1=1週目 2=2週目 3=3週目 4=４周目 0=毎週
+  validates :week, presence: true
+  # dotw は何曜日かを表す e.g. 0=日曜日 1=月曜日 2=火曜日 3=水曜日 4=木曜日 5=金曜日 6=土曜日
   validates :dotw, presence: true
-  validates :start_at, presence: true, uniqueness: { scope: [:dotw, :lesson_class_id] }
-  validates :end_at, presence: true, uniqueness: { scope: [:dotw, :lesson_class_id] }
+  validates :start_at, presence: true, uniqueness: { scope: [:week, :dotw, :lesson_class_id] }
+  validates :end_at, presence: true, uniqueness: { scope: [:week, :dotw, :lesson_class_id] }
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_25_070700) do
+ActiveRecord::Schema.define(version: 2020_11_01_055637) do
 
   create_table "lesson_classes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_25_070700) do
     t.bigint "lesson_class_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "week"
     t.index ["lesson_class_id"], name: "index_lesson_rules_on_lesson_class_id"
   end
 
