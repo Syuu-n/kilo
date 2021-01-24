11ddc42267797a0b167560a580ca7d28cea022ce
Aura-n <reikage46497@gmail.com>
2021-01-24 17:44:15 +0900

    Fix users & trials specs

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     last_name_kana { Faker::Creature::Animal.name }
     birthday { Time.current - rand(1..100).year }
     phone_number { Faker::PhoneNumber.cell_phone }
-    plans { [Plan.all] }
+    plans { Plan.all }
     role { Role.admin }
     confirmed_at { Time.current }
   end
@@ -28,6 +28,20 @@ FactoryBot.define do
   end
 
   factory :trial, class: User do
+    email { Faker::Internet.email }
+    password { 'password' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    first_name_kana { Faker::Ancient.god }
+    last_name_kana { Faker::Creature::Animal.name }
+    birthday { Time.current - rand(1..100).year }
+    phone_number { Faker::PhoneNumber.cell_phone }
+    plans { [Plan.trial_plan] }
+    role { Role.trial }
+    confirmed_at { Time.current }
+  end
+
+  factory :unconfirmed_trial, class: User do
     email { Faker::Internet.email }
     password { 'password' }
     first_name { Faker::Name.first_name }
