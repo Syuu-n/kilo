31e773f337492add65d3d47e7e7a5946b8fa4bfe
Aura-n <reikage46497@gmail.com>
2021-01-30 09:41:49 +0900

    Add prod settings to frontend

---
+++ frontend/webpack.prod.config.ts
@@ -0,0 +1,32 @@
+// import * as path from 'path';
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css?$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: 'file-loader'
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
+  },
+  mode: "production"
+};
