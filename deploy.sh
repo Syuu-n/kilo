ebf51aaa7d6fdba2a39b4046ea0bd06eeb2b65a4
Aura-n <reikage46497@gmail.com>
2021-01-30 22:41:02 +0900

    Fix for deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,9 @@ echo "                                        00
   00000000          0000000000        000000        000000        000000      0000                                                                                                                    
 000000000000      0000000000000000  0000000000    0000000000        000000000000  "
 
+# scp files local to remote
+docker-machine scp -d -r . conoha1:/home/aura/kilo
+
 set -eu
 
 export DEPLOY_DOCKER_MACHINE="conoha1" # Docker Machine Name
@@ -28,8 +31,10 @@ export DEPLOY_DOCKER_MACHINE="conoha1" # Docker Machine Name
 eval $(docker-machine env $DEPLOY_DOCKER_MACHINE)
 
 # Build
-docker-compose -f docker-compose.prod.yml build --no-cache
-docker-compose -f docker-compose.prod.yml up --force-recreate -d --no-deps --remove-orphans
+docker-compose build --no-cache
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate -d --no-deps --remove-orphans
 
 # Reset docker context
 eval $(docker-machine env -u)
+
+echo "Done!"
